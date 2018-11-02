64aaa03276a327ee256d8e8d8adb6d955b40419f
Heverson carvalho <heverson.carvalho15@gmail.com>
2018-11-02 00:21:45 -0400

    Adicona script de bd mysql

---
+++ basic/script2.sql
@@ -0,0 +1,21 @@
+#DROP TABLE aula;
+#DROP TABLE curso;
+
+CREATE TABLE IF NOT EXISTS curso (
+curso_id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+curso_nome VARCHAR(45),
+PRIMARY KEY (curso_id)
+);
+
+
+CREATE TABLE aula (
+aula_id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+aula_nome VARCHAR(45),
+curso_id INT UNSIGNED NOT NULL,
+PRIMARY KEY (aula_id),
+FOREIGN KEY (curso_id) REFERENCES curso (curso_id)
+);
+
+CREATE TABLE cliente(
+cliente_id int not null auto_increment
+);
